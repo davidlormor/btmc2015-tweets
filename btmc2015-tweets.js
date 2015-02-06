d7e1d02f454dbb9c3029aac0f22f8f74ee6980b9
David Lormor <davidlormor@gmail.com>
2015-02-06 16:55:24 +0000

    Capture tweets and display in browser

--- btmc2015-tweets.js
+++ btmc2015-tweets.js
@@ -1,17 +1,12 @@
+Tweets = new Mongo.Collection("tweets");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    tweets: function(){
+      return Tweets.find({}, {sort: {tweetedAt: -1}});
+    },
+    tweetsCount: function(){
+      return Tweets.find({}).count();
     }
   });
 }
@@ -19,5 +14,35 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    var Twit = Meteor.npmRequire('twit');
+    var consumerKey = process.env.TWITTER_CONSUMER_KEY;
+    var consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+    var accessToken = process.env.TWITTER_ACCESS_TOKEN;
+    var accessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+    console.log(consumerKey);
+
+    var T = new Twit({
+        consumer_key:         consumerKey, // API key
+        consumer_secret:      consumerSecret, // API secret
+        access_token:         accessToken, 
+        access_token_secret:  accessTokenSecret
+    });
+
+    // subscribe to #BTMC2015 stream in Montgomery
+    var montgomery = ['-87.2', '31.58', '-85.19', '33.05']
+    var stream = T.stream('statuses/filter', { track: '#btmc2015', language: 'en' });
+
+    stream.on('tweet', Meteor.bindEnvironment(function (tweet) {
+      console.log('new tweet: ' + tweet.id);
+      // save tweet data - id, created_at, text, user.id, user.name, user.screen_name
+      Tweets.insert({
+        tweetId: tweet.id,
+        tweetedAt: tweet.created_at,
+        text: tweet.text,
+        userId: tweet.user.id,
+        userName: tweet.user.name,
+        userScreenName: tweet.user.screen_name
+      });
+    }));
   });
 }
